344e810ea26aecd16787ad585b34957a0ed788d0
Nuno David <ndavidq0@gmail.com>
2024-06-23 20:55:10 +0100

    refactor: move params to outside of prob density fn

--- src/bin/hsp.rs
+++ src/bin/hsp.rs
@@ -15,16 +15,14 @@ fn hsp_prob_density(
     xs: Range<f64>,
     xps: Range<f64>,
     samples: (usize, usize),
+    phi: impl Fn(f64) -> f64 + std::clone::Clone,
+    psi_u: impl Fn(f64) -> f64 + std::clone::Clone + 'static,
+    psi_r: impl Fn(f64) -> f64 + std::clone::Clone + 'static,
 ) -> impl Iterator<Item = (f64, f64, f64)> {
-    let phi = |x: f64| x.powi(2);
     let step = (
         (xs.end - xs.start) / samples.0 as f64,
         (xps.end - xps.start) / samples.1 as f64,
     );
-    let psi_u = move |x: f64| x.gaussian(MU, SIGMA);
-    let psi_r = psi_u;
-    let psi_u_sq = move |x: f64| psi_u(x).powi(2);
-    let psi_r_sq = move |x: f64| psi_r(x).powi(2);
     (0..(samples.0 * samples.1))
         .map(move |k| {
             let x = xs.start + step.0 * (k % samples.0) as f64;
@@ -32,7 +30,8 @@ fn hsp_prob_density(
             (
                 x,
                 xp,
-                0.25 * ((psi_u_sq(x) * psi_r_sq(xp)) + (psi_r_sq(x) * psi_u_sq(xp)))
+                0.25 * ((psi_u(x).powi(2) * psi_r(xp).powi(2))
+                    + (psi_r(x).powi(2) * psi_u(xp).powi(2)))
                     - (V * 0.5
                         * psi_u(x)
                         * psi_u(xp)
@@ -45,17 +44,22 @@ fn hsp_prob_density(
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (phi_str, phi) = ("x^2", |x: f64| x.powi(2));
+    let psi_u = move |x: f64| x.gaussian(MU, SIGMA);
+    let psi_r = psi_u;
     Heatplot2D {
         title: format!(
-            "HSP Probability Density (V = {V}, Φ = x^2, Ψu = Ψr = N({MU}, {}))",
+            "HSP Probability Density (V = {V}, Φ = {phi_str}, Ψu = Ψr = N({MU}, {}))",
             SIGMA.powi(2)
         )
         .as_str()
         .into(),
         out_file_name: "hsp.png",
-        compute_fn: Box::new(|xs: Range<f64>, ys: Range<f64>, samples: (usize, usize)| {
-            Box::new(hsp_prob_density(xs, ys, samples))
-        }),
+        compute_fn: Box::new(
+            move |xs: Range<f64>, ys: Range<f64>, samples: (usize, usize)| {
+                Box::new(hsp_prob_density(xs, ys, samples, phi, psi_u, psi_r))
+            },
+        ),
         x_range: X_RANGE,
         y_range: Y_RANGE,
         ..Default::default()
